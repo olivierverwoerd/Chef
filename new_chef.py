1a159140096db75843a66791fc2633496be32a86
Olivier Verwoerd <olivier.verwoerd@student.hu.nl>
2020-05-22 17:43:32 +0200

    update

---
+++ new_chef.py
@@ -0,0 +1,44 @@
+"""A maybe not so simple chef interpreter made with a functional method"""
+import sys
+from typing import List, Callable
+from preprocessor import preprocess
+
+
+def token_topping(file: str) -> List[str]:
+    input = file.split('\n')
+    tokens = ['NAME']
+    def add_tokens(input: str, tokens: List[str]) -> List[str]:
+        new_tokens = tokens.copy()
+
+    return add_tokens(input, tokens)
+
+def sub_main(file_name: str) -> str:
+    """
+    Main opens the file and runs the code directly
+    :param file_name: The filename in this directory to run.
+
+    note to self: run(parse(lex(text)))
+    """
+    print("Running " + file_name)
+    print("-------------------------------")
+    f = open(file_name, "r")
+    recipe = f.read()
+    s = preprocess(recipe)
+    sf = open(file_name + ".ppf", "w")
+    sf.write(s)
+    sf.close()
+    #print(token_topping(recipe))
+
+
+
+def main():
+    sub_main("pretest.chef")
+    # sub_main("tiny.chef")
+    #assert sub_main("tiny.chef") == "Hello world!"
+    #assert sub_main("run.chef") == "Hello world!"
+    #assert sub_main("higher.chef") == "112233445510203040504" # if 50 is added
+    #assert sub_main("fruit.chef") == "504"
+
+
+if __name__ == "__main__":
+    main()
--- preprocessor.py
+++ preprocessor.py
@@ -24,6 +24,9 @@ def remove_too_many_spaces(line: str) -> str:
 
 def preprocess(file: str) -> str:
     liquids = ["dash", "cup", "l", "ml", "dashes", "cups"]
+    commands = ["put","liquefy","pour","fold","add","remove","combine","divide","stir","mix",
+             "clean","pour","set","refrigerate","serve","take", "zipwith", "foldl", "foldr"]
+    commands_with_double_values = ["zipwith", "pour"]
     def loop(file: str, r: str = "", state = "TITLE"):
         tmp = ''
         #spits according to what's nessesary
@@ -40,24 +43,69 @@ def preprocess(file: str) -> str:
         else:
             tmp = file.split('.', 1)
         line = tmp[0].lower()
-        if state == "TITLE" and line != '\n':
-            r += "TITLE\n"
-            r += line.replace(' ', '_')
-            state = "NONE"
+        if state == "TITLE":
+            print(r[-1:])
+            if r[-1:] == '\n':
+                r = r[:-1]
+            if line != '\n':
+                r += "TITLE "
+                name = line.replace('\n', '')
+                r += name.replace(' ', '_')
+                state = "NONE"
+
 
         if state == "METHOD":
             line = remove_number_extention(line)
             line = line.replace("the mixing bowl", '0')
-            line = line.replace("baking dish", '0')
-            line = re.sub(' (hours|hour|contents of|'
-                          'from refrigerator|minutes|well|contents of|'
-                          'mixing bowl| baking dish|into|the|of |with |for)',
+            line = line.replace("the baking dish", '0')
+            words = line.split(' ')
+            line = re.sub(' (hours|hour|contents of|to '
+                          'from|minutes|well|contents of|refrigerator|from|'
+                          'mixing bowl|baking dish|into|the|of |with |for)',
                           ' ', line)
             line = remove_too_many_spaces(line)
-            words = line.split(' ', 1)
-            print(words)
-            r += words[0].upper() + " "
-            r += words[1]
+            word = line.split(' ', 1)
+            if word[1][-1] == ' ':
+                word[1] = word[1][:-1];
+            if words[0] not in commands and words[0] != '\n':
+                if words[-2] == "until":
+                    r += "LOOP_END "
+                    r += word[1].split(" until")[0].replace(" ", "_")
+                elif words[1] == "the":
+                    r += "LOOP_START "
+                    r += word[1].replace(' ', '_')
+                else:
+                    raise Exception("PREPROCESSOR ERROR:" + str(words))
+            else:
+                if words[0] == 'liquefy':
+                    if words[1] == "contents":
+                        r += "LIQUEFY_CONTENTS "
+                        r += word[1]
+                    else:
+                        r += "LIQUEFY " + word[1].replace(' ', '_')
+                elif words[0] == 'add' and words[1] == "dry":
+                    r += "ADD_DRYS"
+                elif words[0] == 'stir' and words[-3] == "for":
+                    r += "STIR_FOR " + word[1]
+                elif words[0] == 'set' and words[1] == "aside":
+                    r += "ENDLOOP"
+                elif words[0] == "take":
+                    r += "TAKE " + word[1].replace(' ', '_')
+                elif words[0] == "serve":
+                    if words[1] == "with":
+                        r += "SERVE_WITH " + word[1].replace(' ', '_')
+                        r += '\n'
+                        state = "TITLE"
+                    else:
+                        r += '\n'
+                        state = "TITLE"
+                else:
+                    r += word[0].upper() + " "
+                    if word[0] in commands_with_double_values:
+                        r += word[1]
+                    else:
+                        r += word[1].replace(' ', '_', len(word[1].split(' '))-2)
+
 
         if state == "INGREDIENTS":
             if line == "method.":
